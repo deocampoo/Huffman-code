4140e4f3be8ad45102477accbade3901daa12df5
sofia lappano <sofiaselenelappano@gmail.com>
2024-09-17 18:00:25 -0300

    costo de la class Nodo

--- Nodo.java
+++ Nodo.java
@@ -5,7 +5,9 @@ public class Nodo implements  Comparable<Nodo> {
     public Nodo izquierdo, derecho; 
     public String character; 
     
-    //constructor
+    //constructor    
+    //---> costo: O(1): La creación de un nuevo nodo y la asignación de valores a los atributos se realiza en tiempo constante, ya que no depende del tamaño de ninguna colección.
+
     public Nodo(int frecuencia,String character) {
         this.frecuencia = frecuencia;
         this.character = character;
@@ -16,13 +18,17 @@ public class Nodo implements  Comparable<Nodo> {
 
     //hace que se compare en el minHeap por la frecuencia, sino no funciona
     //porque enteinde que es la letra por defecto
+    //---> costo: O(1): La comparación de dos enteros es una operación que se realiza en tiempo constante.
     @Override
     public int compareTo(Nodo otroNodo) {
+        //---> La función Integer.compare() simplemente compara los valores de frecuencia de dos nodos, lo cual no depende del tamaño de la estructura de datos.
         return Integer.compare(this.frecuencia, otroNodo.frecuencia);
     }
 
     
-    //lo uso para imprimir el a:2  b:3
+    //lo uso para imprimir el a:2  b:3 
+    //%s: Inserta una cadena de texto. %d: Inserta un número entero.
+    //--> cosot: O(1): El método accede directamente a los atributos character y frecuencia y no depende de ninguna colección o estructura de datos.
     public String getTotal() {
         return String.format("%s:%d", character, frecuencia);
     }
